2d743bc496645c7e6f621692a603e87aa1e623f5
Feba Rachel Lalu <114395573+Fe195@users.noreply.github.com>
2023-01-03 10:58:44 +0530

    Update store.js

--- src/components/store.js
+++ src/components/store.js
@@ -1,20 +1,25 @@
 import { configureStore, createSlice } from "@reduxjs/toolkit";
 
+//creating slice with reducers
 const slice = createSlice({
-    name: 'hot-news',
-    initialState: {
-        articlesNum: 0,
-        articles : []
+  name: "hot-news",
+  initialState: {
+    articlesNum: 0,
+    articles: [],
+  },
+  reducers: {
+    setnews: (state, action) => {
+      state.articles = [...action.payload];
+      state.articlesNum = action.payload.length;
     },
-    reducers:{
-        
-    }
-})
+  },
+});
 
+//store configration
 const store = configureStore({
-    
-})
+  reducer: { news: slice.reducer },
+});
 
 export default store;
 
-export const actions = slice.actions;
+//exporting slice actions to dispatch actions from other components
